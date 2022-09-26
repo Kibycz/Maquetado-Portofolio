78a96a7ad40e9af6ed6c71c92a715c21107e0a84
Kibycz <villalbajoakin@gmail.com>
2022-09-25 21:37:29 -0300

    componentes completos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { HeaderComponent } from './component/header/header.component';
+import { BodyComponent } from './component/body/body.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
