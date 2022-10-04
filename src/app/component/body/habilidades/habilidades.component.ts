46ab001e8c48793c1b568ea213ac33a972751c1a
Kibycz <villalbajoakin@gmail.com>
2022-10-04 13:09:00 -0300

    Graficos sin terminar

--- src/app/component/body/habilidades/habilidades.component.ts
+++ src/app/component/body/habilidades/habilidades.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, setTestabilityGetter } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-habilidades',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./habilidades.component.css']
 })
 export class HabilidadesComponent implements OnInit {
+  canvas: any;
+  ctx:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setBar();
+  }
+
+  setBar() {
+    this.canvas = document.getElementById('HardSkills');
+
+    this.ctx = this.canvas.getContext('2d');
+
+    const HardSkills = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Frontend', 'Backend', 'Servidores', 'Base de Datos'],
+          datasets: [{
+              label: 'Hard Skills',
+              data: [75, 50, 25, 10],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          responsive: true,
+        }
+    });
   }
 
 }
